0a48a0e5cf918c2c947985492b49326dc5f6964a
Sahil Gedam <119723149+xahilg18@users.noreply.github.com>
2022-12-22 19:58:22 +0530

    Add files via upload

---
+++ startupxhl.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+wcm="wellcome Sahil"
+
+
+
+cowsay $wcm
+date
+figlet sahil | pv -qL 100 
+
+
+df -h | grep used ; df -h | grep Use% ; df -h | grep size ; df -h |  grep /dev/mapper/data
